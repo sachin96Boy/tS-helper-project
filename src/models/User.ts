c597f9d0a7d85b4fd3b12d2d46f9d518737f6d99
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-07-29 13:29:46 +0530

    chor: user module declaration

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,15 +3,29 @@ import { faker } from '@faker-js/faker';
 // import { fakerDE as faker } from '@faker-js/faker';
 
 class User {
-    private const name: string;
-    private const location: {
+    private  name: string;
+    private  location: {
         lat:number;
-        lng:number
+        lng:number;
     };
 
 
     constructor(){
-        
+        this.name = faker.person.firstName('male');
+        this.location = {
+            lat: faker.location.latitude(),
+            lng: faker.location.longitude()
+        }
+    }
+
+    get getName(){
+        return this.name;
+    }
+
+    get getLocation(){
+        return this.location;
     }
 
 }
+
+export default User;
