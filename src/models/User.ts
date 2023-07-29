5fa194817afbc55e62d982a363c1ed3c525e799c
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-07-29 13:09:33 +0530

    chor: faker

---
+++ src/models/User.ts
@@ -0,0 +1,17 @@
+import { faker } from '@faker-js/faker';
+// or, if desiring a different locale
+// import { fakerDE as faker } from '@faker-js/faker';
+
+class User {
+    private const name: string;
+    private const location: {
+        lat:number;
+        lng:number
+    };
+
+
+    constructor(){
+        
+    }
+
+}
